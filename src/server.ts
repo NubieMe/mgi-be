9811ac76178318be7e6e73e1c3ae70ec8efdedbd
NubieMe <fahmi.mellow@gmail.com>
2024-07-08 19:35:02 +0700

    initialize

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { routes } from "./route";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", routes);
+
+app.listen(3000, () => {
+    console.log("server running on port 3000");
+});
